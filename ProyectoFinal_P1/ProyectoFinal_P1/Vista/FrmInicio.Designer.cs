cc0e7b3b51a8064288db5c7a10d2dc6ae9849930
carlosjavierjimenezvasquez <69540480+carlosjavierjimenezvasquez@users.noreply.github.com>
2021-04-23 18:56:11 -0600

    Apariencia

---
+++ ProyectoFinal_P1/ProyectoFinal_P1/Vista/FrmInicio.Designer.cs
@@ -0,0 +1,155 @@
+﻿
+namespace ProyectoFinal_P1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnAdminsitrador = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.btnProfesor = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.btnAdminsitrador);
+            this.panel1.Location = new System.Drawing.Point(12, 108);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(276, 314);
+            this.panel1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::ProyectoFinal_P1.Properties.Resources.kisspng_computer_icons_computer_network_system_administrat_personal_configuration_svg_png_icon_free_download_5c2efde600a146_7683610715465835260026__2_;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(270, 267);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnAdminsitrador
+            // 
+            this.btnAdminsitrador.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Black;
+            this.btnAdminsitrador.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Black;
+            this.btnAdminsitrador.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAdminsitrador.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.btnAdminsitrador.Location = new System.Drawing.Point(80, 276);
+            this.btnAdminsitrador.Name = "btnAdminsitrador";
+            this.btnAdminsitrador.Size = new System.Drawing.Size(113, 35);
+            this.btnAdminsitrador.TabIndex = 3;
+            this.btnAdminsitrador.Text = "ADMINISTRADOR";
+            this.btnAdminsitrador.UseVisualStyleBackColor = true;
+            this.btnAdminsitrador.Click += new System.EventHandler(this.btnAdminsitrador_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.pictureBox2);
+            this.panel2.Controls.Add(this.btnProfesor);
+            this.panel2.Location = new System.Drawing.Point(324, 108);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(286, 314);
+            this.panel2.TabIndex = 1;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::ProyectoFinal_P1.Properties.Resources._46139;
+            this.pictureBox2.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(280, 267);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 5;
+            this.pictureBox2.TabStop = false;
+            // 
+            // btnProfesor
+            // 
+            this.btnProfesor.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Black;
+            this.btnProfesor.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Black;
+            this.btnProfesor.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnProfesor.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.btnProfesor.Location = new System.Drawing.Point(98, 276);
+            this.btnProfesor.Name = "btnProfesor";
+            this.btnProfesor.Size = new System.Drawing.Size(113, 35);
+            this.btnProfesor.TabIndex = 0;
+            this.btnProfesor.Text = "PROFESOR";
+            this.btnProfesor.UseVisualStyleBackColor = true;
+            this.btnProfesor.Click += new System.EventHandler(this.btnProfesor_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.LightGray;
+            this.label1.Location = new System.Drawing.Point(112, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(345, 27);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "CÓMO QUIERES INICIAR SECIÓN";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(75)))), ((int)(((byte)(75)))), ((int)(((byte)(75)))));
+            this.ClientSize = new System.Drawing.Size(665, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnAdminsitrador;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button btnProfesor;
+    }
+}
+
---
+++ ProyectoFinal_P1/ProyectoFinal_P1/Vista/FrmInicio.cs
@@ -0,0 +1,35 @@
+﻿using ProyectoFinal_P1.Vista;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProyectoFinal_P1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdminsitrador_Click(object sender, EventArgs e)
+        {
+            FrmLogin login = new FrmLogin();
+            login.Show();
+            this.Hide();
+        }
+
+        private void btnProfesor_Click(object sender, EventArgs e)
+        {
+            FrmLogin login = new FrmLogin();
+            login.Show();
+            this.Hide();
+        }
+    }
+}
